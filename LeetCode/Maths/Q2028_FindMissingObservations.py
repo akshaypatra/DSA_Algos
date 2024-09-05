c115caaf2c67a6c5eb3283f07a026ac092586a21
Akshay Patra <akshay.patra114@gmail.com>
2024-09-05 21:26:26 +0530

    Q2028 missing observations problem . used a spcific formula to tackle this

---
+++ LeetCode/Maths/Q2028_FindMissingObservations.py
@@ -0,0 +1,57 @@
+'''
+You have observations of n + m 6-sided dice rolls with each face numbered from 1 to 6. n of the observations went missing, and you only have the observations of m rolls. Fortunately, you have also calculated the average value of the n + m rolls.
+You are given an integer array rolls of length m where rolls[i] is the value of the ith observation. You are also given the two integers mean and n.
+
+Return an array of length n containing the missing observations such that the average value of the n + m rolls is exactly mean. If there are multiple valid answers, return any of them. If no such array exists, return an empty array.
+
+The average value of a set of k numbers is the sum of the numbers divided by k.
+
+Note that mean is an integer, so the sum of the n + m rolls should be divisible by n + m.
+
+ 
+
+Example 1:
+
+Input: rolls = [3,2,4,3], mean = 4, n = 2
+Output: [6,6]
+Explanation: The mean of all n + m rolls is (3 + 2 + 4 + 3 + 6 + 6) / 6 = 4.
+Example 2:
+
+Input: rolls = [1,5,6], mean = 3, n = 4
+Output: [2,3,2,2]
+Explanation: The mean of all n + m rolls is (1 + 5 + 6 + 2 + 3 + 2 + 2) / 7 = 3.
+Example 3:
+
+Input: rolls = [1,2,3,4], mean = 6, n = 4
+Output: []
+Explanation: It is impossible for the mean to be 6 no matter what the 4 missing rolls are.
+'''
+
+
+#Solution:
+
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        m=len(rolls)
+        total_sum=(m+n)*mean
+
+        missing_sum= total_sum - sum(rolls) 
+
+        # validation
+
+        if missing_sum>((n)*6) or missing_sum<n:
+            return []
+        
+        # simulation
+        
+        res=[]
+        while n:
+            dice=min(6,missing_sum-n+1)
+            #formula for finding elements  min(6,missing_sum-n+1)  ,as max can be 6 
+
+            res.append(dice)
+            missing_sum-=dice
+            n-=1
+
+        
+        return res
