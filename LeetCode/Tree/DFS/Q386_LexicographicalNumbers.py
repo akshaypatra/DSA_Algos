471c505e8f35ee75e5bcc0381a34eb1df40ca83a
Akshay Patra <akshay.patra114@gmail.com>
2024-09-21 20:32:45 +0530

    solved 386 lexicographical problem using dfs

---
+++ LeetCode/Tree/DFS/Q386_LexicographicalNumbers.py
@@ -0,0 +1,38 @@
+'''
+leetcode 386 : lexicographical numbers
+
+Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+
+You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
+
+ 
+
+Example 1:
+
+Input: n = 13
+Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+Example 2:
+
+Input: n = 2
+Output: [1,2]
+
+'''
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        result = []
+
+        def dfs(current):
+            if current > n:
+                return
+            result.append(current)
+            for i in range(10):
+                next_num = current * 10 + i
+                if next_num > n:
+                    break
+                dfs(next_num)
+
+        for i in range(1, 10):
+            dfs(i)
+        
+        return result
