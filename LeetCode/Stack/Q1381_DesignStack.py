2a1eb4c4379e4f79f84d160a1f1d5d8d3d5a770a
Akshay Patra <akshay.patra114@gmail.com>
2024-09-30 21:53:36 +0530

    solved 1381 , stack implimentation  problem

---
+++ LeetCode/Stack/Q1381_DesignStack.py
@@ -0,0 +1,80 @@
+'''
+
+1381. Design a Stack With Increment Operation
+
+Medium
+Topics : Array , Stack
+
+Implement the CustomStack class:
+
+CustomStack(int maxSize) Initializes the object with maxSize which is the maximum number of elements in the stack.
+void push(int x) Adds x to the top of the stack if the stack has not reached the maxSize.
+int pop() Pops and returns the top of the stack or -1 if the stack is empty.
+void inc(int k, int val) Increments the bottom k elements of the stack by val. If there are less than k elements in the stack, increment all the elements in the stack.
+ 
+
+Example 1:
+
+Input
+["CustomStack","push","push","pop","push","push","push","increment","increment","pop","pop","pop","pop"]
+[[3],[1],[2],[],[2],[3],[4],[5,100],[2,100],[],[],[],[]]
+Output
+[null,null,null,2,null,null,null,null,null,103,202,201,-1]
+Explanation
+CustomStack stk = new CustomStack(3); // Stack is Empty []
+stk.push(1);                          // stack becomes [1]
+stk.push(2);                          // stack becomes [1, 2]
+stk.pop();                            // return 2 --> Return top of the stack 2, stack becomes [1]
+stk.push(2);                          // stack becomes [1, 2]
+stk.push(3);                          // stack becomes [1, 2, 3]
+stk.push(4);                          // stack still [1, 2, 3], Do not add another elements as size is 4
+stk.increment(5, 100);                // stack becomes [101, 102, 103]
+stk.increment(2, 100);                // stack becomes [201, 202, 103]
+stk.pop();                            // return 103 --> Return top of the stack 103, stack becomes [201, 202]
+stk.pop();                            // return 202 --> Return top of the stack 202, stack becomes [201]
+stk.pop();                            // return 201 --> Return top of the stack 201, stack becomes []
+stk.pop();                            // return -1 --> Stack is empty return -1.
+
+'''
+
+
+
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.stack=[None]*maxSize
+        self.maxSize=maxSize
+        self.top=-1
+        
+
+    def push(self, x: int) -> None:
+        if self.top< self.maxSize-1:
+            self.top+=1
+            self.stack[self.top]=x
+        
+        
+
+    def pop(self) -> int:
+        if self.top == -1:
+            return -1
+        val = self.stack[self.top] 
+        self.stack[self.top] = 0
+        self.top -= 1
+        return val
+
+        
+
+    def increment(self, k: int, val: int) -> None:
+        limit = min(k, self.top + 1)  # Increment up to the top or k elements
+        for i in range(limit):
+            self.stack[i] += val
+        
+
+        
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
