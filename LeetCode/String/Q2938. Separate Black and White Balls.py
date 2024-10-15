e920223990b2f6047464f5eca4201fc200d756bb
Akshay Patra <akshay.patra114@gmail.com>
2024-10-15 11:15:18 +0530

    solved 2938 Separate Black and White Balls . Used hint to solve the problem

---
+++ LeetCode/String/Q2938. Separate Black and White Balls.py
@@ -0,0 +1,72 @@
+'''
+2938. Separate Black and White Balls
+
+Medium
+
+There are n balls on a table, each ball has a color black or white.
+
+You are given a 0-indexed binary string s of length n, where 1 and 0 represent black and white balls, respectively.
+
+In each step, you can choose two adjacent balls and swap them.
+
+Return the minimum number of steps to group all the black balls to the right and all the white balls to the left.
+
+ 
+
+Example 1:
+
+Input: s = "101"
+Output: 1
+Explanation: We can group all the black balls to the right in the following way:
+- Swap s[0] and s[1], s = "011".
+Initially, 1s are not grouped together, requiring at least 1 step to group them to the right.
+Example 2:
+
+Input: s = "100"
+Output: 2
+Explanation: We can group all the black balls to the right in the following way:
+- Swap s[0] and s[1], s = "010".
+- Swap s[1] and s[2], s = "001".
+It can be proven that the minimum number of steps needed is 2.
+Example 3:
+
+Input: s = "0111"
+Output: 0
+Explanation: All the black balls are already grouped to the right.
+ 
+
+Constraints:
+
+1 <= n == s.length <= 105
+s[i] is either '0' or '1'.
+'''
+
+
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+
+        '''
+        Iterate right to left and count the number of 0 that have already occurred, whenever you iterate on 
+        1  add that counter to the answer.
+        '''
+
+        i=len(s)-1
+        zero_count=0
+        res=0
+        while i>=0:
+            if s[i]=='0':
+                zero_count+=1
+                i-=1
+
+            elif s[i]=='1' and zero_count>0:
+                res+=zero_count
+                i-=1
+            else:
+                i-=1
+
+        return res
+            
+            
+            
+                
+            
