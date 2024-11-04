b334cae2aaf48b1d88d82dcfe2b178d61b9a5f99
Akshay Patra <akshay.patra114@gmail.com>
2024-11-04 12:03:24 +0530

    3163. String Compression III

---
+++ Binary Trees/Implementaiton.py
@@ -0,0 +1,87 @@
+class Node: 
+    # Implement a node of the binary search tree.
+    # Constructor for a node with key and a given parent
+    # parent can be None for a root node.
+    def __init__(self, key, parent = None): 
+        self.key = key
+        self.parent = parent 
+        self.left = None # We will set left and right child to None
+        self.right = None
+        # Make sure that the parent's left/right pointer
+        # will point to the newly created node.
+        if parent != None:
+            if key < parent.key:
+                assert(parent.left == None), 'parent already has a left child -- unable to create node'
+                parent.left = self
+            else: 
+                assert key > parent.key, 'key is same as parent.key. We do not allow duplicate keys in a BST since it breaks some of the algorithms.'
+                assert(parent.right == None ), 'parent already has a right child -- unable to create node'
+                parent.right = self
+        
+    # Utility function that keeps traversing left until it finds 
+    # the leftmost descendant
+    def get_leftmost_descendant(self):
+        if self.left != None:
+            return self.left.get_leftmost_descendant()
+        else:
+            return self
+    
+    # TODO: Complete the search algorithm below
+    # You can call search recursively on left or right child
+    # as appropriate.
+    # If search succeeds: return a tuple True and the node in the tree
+    # with the key we are searching for.
+    # Also note that if the search fails to find the key 
+    # you should return a tuple False and the node which would
+    # be the parent if we were to insert the key subsequently.
+    def search(self, key):
+        if self.key == key: 
+            return (True, self)
+        # your code here
+        
+    
+    #TODO: Complete the insert algorithm below
+    # To insert first search for it and find out
+    # the parent whose child the currently inserted key will be.
+    # Create a new node with that key and insert.
+    # return None if key already exists in the tree.
+    # return the new node corresponding to the inserted key otherwise.
+    def insert(self, key):
+        # your code here
+        
+        
+        
+    # TODO: Complete algorithm to compute height of the tree
+    # height of a node whose children are both None is defined
+    # to be 1.
+    # height of any other node is 1 + maximum of the height 
+    # of its children.
+    # Return a number that is th eheight.
+
+    def height(self):
+        # your code here
+        
+    
+    #TODO: Write an algorithm to delete a key in the tree.
+    # First, find the node in the tree with the key.
+    # Recommend drawing pictures to visualize these cases below before
+    # programming.
+    # Case 1: both children of the node are None
+    #   -- in this case, deletion is easy: simply find out if the node with key is its
+    #      parent's left/right child and set the corr. child to None in the parent node.
+    # Case 2: one of the child is None and the other is not.
+    #   -- replace the node with its only child. In other words,
+    #      modify the parent of the child to be the to be deleted node's parent.
+    #      also change the parent's left/right child appropriately.
+    # Case 3: both children of the parent are not None.
+    #    -- first find its successor (go one step right and all the way to the left).
+    #    -- function get_leftmost_descendant may be helpful here.
+    #    -- replace the key of the node by its successor.
+    #    -- delete the successor node.
+    # return: no return value specified
+    
+    def delete(self, key):
+        (found, node_to_delete) = self.search(key)
+        assert(found == True), f"key to be deleted:{key}- does not exist in the tree"
+        # your code here
+        
