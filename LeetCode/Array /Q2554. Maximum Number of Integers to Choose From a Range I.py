889fc98da7dfef045c57c7b294e8318bc5c59ead
Akshay Patra <akshay.patra114@gmail.com>
2024-12-06 17:35:04 +0530

    2554. Maximum Number of Integers to Choose From a Range I

--- LeetCode/Array /Q2554. Maximum Number of Integers to Choose From a Range I.py
+++ LeetCode/Array /Q2554. Maximum Number of Integers to Choose From a Range I.py
@@ -12,19 +12,19 @@ The chosen integers should not be in the array banned.
 The sum of the chosen integers should not exceed maxSum.
 Return the maximum number of integers you can choose following the mentioned rules.
 
- 
-
 Example 1:
 
 Input: banned = [1,6,5], n = 5, maxSum = 6
 Output: 2
 Explanation: You can choose the integers 2 and 4.
 2 and 4 are from the range [1, 5], both did not appear in banned, and their sum is 6, which did not exceed maxSum.
+
 Example 2:
 
 Input: banned = [1,2,3,4,5,6,7], n = 8, maxSum = 1
 Output: 0
 Explanation: You cannot choose any integer while following the mentioned conditions.
+
 Example 3:
 
 Input: banned = [11], n = 7, maxSum = 50
