3190de23543fee511bf41a175c0511c1e6ff0a4d
Akshay Patra <akshay.patra114@gmail.com>
2025-04-04 21:30:09 +0530

    733. Flood Fill

---
+++ LeetCode/Matrices/BFS/Q733. Flood Fill.py
@@ -0,0 +1,67 @@
+'''
+
+733. Flood Fill
+Easy
+Topics
+
+You are given an image represented by an m x n grid of integers image, where image[i][j] represents the pixel value of the image. You are also given three integers sr, sc, and color. Your task is to perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill:
+
+Begin with the starting pixel and change its color to color.
+Perform the same process for each pixel that is directly adjacent (pixels that share a side with the original pixel, either horizontally or vertically) and shares the same color as the starting pixel.
+Keep repeating this process by checking neighboring pixels of the updated pixels and modifying their color if it matches the original color of the starting pixel.
+The process stops when there are no more adjacent pixels of the original color to update.
+Return the modified image after performing the flood fill.
+
+ 
+
+Example 1:
+
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+
+Explanation:
+
+
+
+From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+
+Note the bottom corner is not colored 2, because it is not horizontally or vertically connected to the starting pixel.
+
+Example 2:
+
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+
+Output: [[0,0,0],[0,0,0]]
+
+Explanation:
+
+The starting pixel is already colored with 0, which is the same as the target color. Therefore, no changes are made to the image.
+
+'''
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        ROWS, COLS = len(image), len(image[0])
+        original_color = image[sr][sc]
+        
+        # If the starting pixel is already the new color, return the image as-is.
+        if original_color == color:
+            return image
+
+        def dfs(r, c):
+            # Base case: If out of bounds or color is not the original, return
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or image[r][c] != original_color:
+                return
+            
+            # Change color and explore neighbors
+            image[r][c] = color
+            for nr, nc in [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]:
+                dfs(nr, nc)
+
+        dfs(sr, sc)
+        return image
+            
+            
