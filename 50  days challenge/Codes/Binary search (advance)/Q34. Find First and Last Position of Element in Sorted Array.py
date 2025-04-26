6c6ca7abfe0d99eaf19e13aa08eb64d57b0e2c5f
Akshay Patra <akshay.patra114@gmail.com>
2025-04-26 11:55:59 +0530

    34. Find First and Last Position of Element in Sorted Array

---
+++ 50  days challenge/Codes/Binary search (advance)/Q34. Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,86 @@
+'''
+34. Find First and Last Position of Element in Sorted Array
+Solved
+Medium
+
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+
+'''
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+
+        """
+        Performing binary serach two times :
+           -> first to find rightmost element (rightBiased)
+           -> second to find the left most element (leftBiased)
+
+        """
+        # leftBias=[True/False] , if false , res is rightBiased
+        def binSearch(leftBias):
+            l,r = 0,len(nums)-1
+
+            i=-1
+            while l<=r:
+                m=(l+r)//2
+
+                if target>nums[m]:
+                    l=m+1
+                elif target < nums[m]:
+                    r = m-1
+                else:
+                    i=m
+                    if leftBias:
+                        r = m-1
+                    else:
+                        l = m+1
+            return i
+        
+        left=binSearch(True)
+        right=binSearch(False)
+
+        return [left,right]
+        
+        
+
+        
+
+
+
+
+
+        
+
+
+        
+
+                
+            
