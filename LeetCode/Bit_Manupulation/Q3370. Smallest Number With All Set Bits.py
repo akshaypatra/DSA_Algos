7bc194b7ebda5f183bedd441783a25bf2782e825
Akshay Patra <akshay.patra114@gmail.com>
2025-10-31 22:57:42 +0530

    3289. The Two Sneaky Numbers of Digitville

--- LeetCode/Bit_Manupulation/Q3370. Smallest Number With All Set Bits.py
+++ LeetCode/Bit_Manupulation/Q3370. Smallest Number With All Set Bits.py
@@ -6,7 +6,6 @@ Easy
 You are given a positive number n.
 
 Return the smallest number x greater than or equal to n, such that the binary representation of x contains only set bits
-
  
 
 Example 1:
