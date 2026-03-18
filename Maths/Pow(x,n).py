b4c6ce8d7abb1e74e9962d5e22b08100bf2be240
Akshay Patra <akshay.patra114@gmail.com>
2026-03-18 22:57:27 +0530

    50. Pow(x, n) - Recursion

---
+++ Maths/Pow(x,n).py
@@ -0,0 +1,21 @@
+def pow(x,n):
+    def calc_power(x, n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+            
+            res = calc_power(x, n // 2)
+            res = res * res
+
+            if n % 2 == 1:
+                return res * x
+            
+            return res
+
+    ans = calc_power(x, abs(n))
+
+    if n >= 0:
+        return ans
+        
+    return 1 / ans
